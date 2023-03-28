208b1b93ec9dbd2461f8623770ec473b8b5290dd
Maurício Maia <mauriciomaiacv@gmail.com>
2023-03-28 08:07:38 -0300

    Manipulação do Banco de Dados

--- db-pedidos.go
+++ db-pedidos.go
@@ -23,3 +23,13 @@ func main() {
         NomeCliente  string  `db:"nome_cliente"`
         Valor        float64 `db:"valor"`
     }
+
+    func createPedido(db *sqlx.DB, p *Pedido) error {
+        query := `INSERT INTO pedidos (nome_cliente, valor) VALUES ($1, $2) RETURNING id`
+        return db.QueryRowx(query, p.NomeCliente, p.Valor).Scan(&p.ID)
+    }
+    
+    func getPedido(db *sqlx.DB, id int64) (*Pedido, error) {
+        p := &Pedido{}
+        err := db.Get(p, "SELECT * FROM pedidos WHERE id=$1", id)
+    }
